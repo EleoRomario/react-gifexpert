e5607a92655306b65fd63ad65e0bafed854070ee
eleo huayhua <ehuayhuap@unsa.edu.pe>
2021-09-02 18:58:27 -0500

    docs agregado

---
+++ docs/precache-manifest.13f32d65e61976974e3349a4d8e5f49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae006fe633f84f189081ea92ce8e1a9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e13f62601011d02572a",
+    "url": "/static/css/main.b5cffe97.chunk.css"
+  },
+  {
+    "revision": "84a1ed7605156b1efa12",
+    "url": "/static/js/2.ac4044b4.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.ac4044b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e13f62601011d02572a",
+    "url": "/static/js/main.d323669e.chunk.js"
+  },
+  {
+    "revision": "851e4c3c4aa894498d54",
+    "url": "/static/js/runtime-main.8ac25dc8.js"
+  }
+]);
